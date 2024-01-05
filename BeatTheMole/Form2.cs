f438bcd6afb7e407409e28557793d3ba422d23ba
Ilya Baretkin <usaasu386@mail.ru>
2024-01-05 21:13:09 +0700

    made the Enemy abstract class and Mole and Bomb successor classes

---
+++ BeatTheMole/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeatTheMole
+{
+    public partial class GameForm : Form
+    {
+        public GameForm(string dif_level)
+        {
+            InitializeComponent();
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            // Обратный отсчёт
+            if (CountdownLabel.Visible)
+            {
+                if (CountdownLabel.Text == "1")
+                {
+                    CountdownLabel.Visible = false;
+                    StartLabel.Visible = true;
+                }
+                else
+                {
+                    int countdown_value = int.Parse(CountdownLabel.Text);
+                    --countdown_value;
+                    CountdownLabel.Text = countdown_value.ToString();
+                }
+            }
+            // Игровой процесс
+            else
+            {
+                StartLabel.Visible = false;
+                GameTimer.Stop();
+            }
+        }
+    }
+}
